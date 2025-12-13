0e3486346758422d720da918256f0f2dafb102f2
Fabio Siliberto <fabio.siliberto@gmail.com>
2025-12-13 13:33:16 +0100

    Add UI and form functionality for managing next lottery extractions in LotterySettings component

--- src/app/lottery-settings/lottery-settings.service.ts
+++ src/app/lottery-settings/lottery-settings.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { manageHttpResponse } from '../utils';
-import { AppResponse, LotteryInfo } from '../models';
+import { AppResponse, ExtractionInfo, LotteryInfo } from '../models';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +8,15 @@ import { AppResponse, LotteryInfo } from '../models';
 export class LotterySettingsService {
   async getLotterySettings(id: string): Promise<AppResponse<LotteryInfo>> {
     const res = await fetch('api/lotteries/' + id);
+    manageHttpResponse(res);
+    return await res.json();
+  }
+
+  async saveNextExtraction(extraction: ExtractionInfo) {
+    const res = await fetch('api/lotteries/' + extraction.lotteryId + '/next-extraction', {
+      method: 'POST',
+      body: JSON.stringify(extraction),
+    });
     manageHttpResponse(res);
     return await res.json();
   }
--- src/app/lottery-settings/lottery-settings.ts
+++ src/app/lottery-settings/lottery-settings.ts
@@ -1,14 +1,34 @@
-import { Component, computed, inject, resource } from '@angular/core';
+import { Component, computed, inject, resource, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { LotterySettingsService } from './lottery-settings.service';
 import { ProgressSpinner } from 'primeng/progressspinner';
 import { Button } from 'primeng/button';
 import { AppMessagesService } from '../app-messages/app-messages.service';
+import { DatePicker } from 'primeng/datepicker';
+import { FloatLabel } from 'primeng/floatlabel';
+import { Message } from 'primeng/message';
+import { InputText } from 'primeng/inputtext';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { catchError, map, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Fieldset } from 'primeng/fieldset';
+import { addMinutes } from 'date-fns';
 
 @Component({
   selector: 'app-lottery-settings',
-  imports: [ProgressSpinner, Button, RouterLink],
+  imports: [
+    ProgressSpinner,
+    Button,
+    RouterLink,
+    DatePicker,
+    FloatLabel,
+    Message,
+    InputText,
+    ReactiveFormsModule,
+    AsyncPipe,
+    Fieldset,
+  ],
   templateUrl: './lottery-settings.html',
   styleUrl: './lottery-settings.scss',
 })
@@ -29,9 +49,54 @@ export class LotterySettings {
         const resp = await this.lotterySettingsService
           .getLotterySettings(id)
           .catch(this.appMessagesService.showHttpError());
-        return resp!.data;
+        const lottery = resp!.data;
+
+        if (lottery.nextExtraction) {
+          this.nextExtractionForm.setValue({
+            extractionTime: new Date(lottery.nextExtraction.extractionTime),
+            winningNumbers: lottery.nextExtraction.winningNumbers.join(', '),
+          });
+        }
+        return lottery;
       }
       throw new Error('Lottery ID not provided');
     },
   });
+  protected savingNextExtraction = signal(false);
+
+  protected readonly nextExtractionForm = inject(FormBuilder).nonNullable.group({
+    extractionTime: [addMinutes(new Date(), 30), [Validators.required]],
+    winningNumbers: ['', [Validators.required, Validators.pattern(/^\s*\d?\d\s*(,\s*\d?\d\s*)*$/)]],
+  });
+
+  protected async saveNextExtraction() {
+    const extraction = this.nextExtractionForm.value;
+    const winningNumbers = extraction.winningNumbers!.split(',').map((n) => parseInt(n.trim(), 10));
+    const extractionTime = extraction.extractionTime!.toISOString();
+    const lotteryId = this.lotteryId()!;
+
+    this.savingNextExtraction.set(true);
+    await this.lotterySettingsService
+      .saveNextExtraction({
+        lotteryId,
+        extractionTime,
+        winningNumbers,
+      })
+      .then(this.appMessagesService.showHttpResponse())
+      .catch(this.appMessagesService.showHttpError({ dontThrow: true }));
+    this.savingNextExtraction.set(false);
+  }
+
+  protected currentNumbersInserted$ = this.nextExtractionForm
+    .get('winningNumbers')!
+    .valueChanges.pipe(
+      map(
+        (numbers) =>
+          numbers
+            .split(',')
+            .filter((n) => n.trim() !== '')
+            .map((n) => parseInt(n.trim(), 10)).length,
+      ),
+      catchError((err) => of(null)),
+    );
 }
