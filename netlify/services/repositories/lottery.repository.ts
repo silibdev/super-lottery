8995ffffae418f39b6b15c9a39ca096a7e23d941
Fabio Siliberto <fabio.siliberto@gmail.com>
2025-12-16 12:27:54 +0100

    Update lottery.repository.ts

--- netlify/services/repositories/lottery.repository.ts
+++ netlify/services/repositories/lottery.repository.ts
@@ -2,52 +2,52 @@ import { LotteriesParticipant, LotteryInfo, LotteryOwner } from '../../../src/ap
 import { getStore } from '@netlify/blobs';
 
 export class LotteryRepository {
-  private static readonly LOTTERIES_STORE = getStore({ name: 'lotteries', consistency: 'strong' });
-  private static readonly LOTTERIES_OWNERS_STORE = getStore({
+  private static readonly LOTTERIES_STORE = () => getStore({ name: 'lotteries', consistency: 'strong' });
+  private static readonly LOTTERIES_OWNERS_STORE = () => getStore({
     name: 'lotteries-owners',
     consistency: 'strong',
   });
-  private static readonly LOTTERIES_PARTICIPANT_STORE = getStore({
+  private static readonly LOTTERIES_PARTICIPANT_STORE = () => getStore({
     name: 'lotteries-participant',
     consistency: 'strong',
   });
-  private static readonly CLIENT_NAMES_STORE = getStore({
+  private static readonly CLIENT_NAMES_STORE = () => getStore({
     name: 'client-names',
     consistency: 'strong',
   });
 
   // Lotteries
   static async getLottery(id: string): Promise<LotteryInfo | undefined> {
-    return await this.LOTTERIES_STORE.get(id, { type: 'json' });
+    return await this.LOTTERIES_STORE().get(id, { type: 'json' });
   }
 
   static async saveLottery(id: string, lottery: LotteryInfo): Promise<void> {
-    await this.LOTTERIES_STORE.setJSON(id, lottery);
+    await this.LOTTERIES_STORE().setJSON(id, lottery);
   }
 
   // Owners
   static async getOwner(id: string): Promise<LotteryOwner | undefined> {
-    return await this.LOTTERIES_OWNERS_STORE.get(id, { type: 'json' });
+    return await this.LOTTERIES_OWNERS_STORE().get(id, { type: 'json' });
   }
 
   static async saveOwner(id: string, owner: LotteryOwner): Promise<void> {
-    await this.LOTTERIES_OWNERS_STORE.setJSON(id, owner);
+    await this.LOTTERIES_OWNERS_STORE().setJSON(id, owner);
   }
 
   // Participants
   static async getParticipant(id: string): Promise<LotteriesParticipant | undefined> {
-    return await this.LOTTERIES_PARTICIPANT_STORE.get(id, { type: 'json' });
+    return await this.LOTTERIES_PARTICIPANT_STORE().get(id, { type: 'json' });
   }
 
   static async saveParticipant(id: string, participant: LotteriesParticipant): Promise<void> {
-    await this.LOTTERIES_PARTICIPANT_STORE.setJSON(id, participant);
+    await this.LOTTERIES_PARTICIPANT_STORE().setJSON(id, participant);
   }
 
   static async getClientName(clientId: string): Promise<string | undefined> {
-    return await this.CLIENT_NAMES_STORE.get(clientId, { type: 'text' });
+    return await this.CLIENT_NAMES_STORE().get(clientId, { type: 'text' });
   }
 
   static async saveClientName(clientId: string, name: string): Promise<void> {
-    await this.CLIENT_NAMES_STORE.set(clientId, name);
+    await this.CLIENT_NAMES_STORE().set(clientId, name);
   }
 }
